a7bc19fd49c6ee67ff1fe629dcad9a487d389c5f
javierblancofernandez <javibf50@hotmail.com>
2020-02-11 17:06:40 +0100

    connecting with react-redux Provider in index and App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React, { Component } from "react";
+import {connect} from 'react-redux';
 import Paper from "@material-ui/core/Paper";
 import AppBar from "@material-ui/core/AppBar";
 import Typography from "@material-ui/core/Typography";
 import Toolbar from "@material-ui/core/Toolbar";
-import { createStore } from "redux";
 import { Grid, Col, Row } from "react-flexbox-grid";
 import LocationList from "./components/LocationList";
 import ForecastExtended from "./components/ForecastExtended";
 import {setCity} from './actions';
+
 import "./App.css";
 
-const store = createStore(() => {},
-window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-// este es un actionCreator que le llamamos setCity
+
+
 
 const cities = ["Buenos Aires,ar", "New York,us", "Madrid,es", "Lima,pe"];
+
 
 class App extends Component {
   constructor() {
@@ -30,7 +31,7 @@ class App extends Component {
       city
     });
     //const action = { type: "setCity", value: city };
-    store.dispatch(setCity(city));
+    this.props.setCity(city);
   };
   render() {
     const { city } = this.state;
@@ -69,4 +70,11 @@ class App extends Component {
   }
 }
 
-export default App;
+
+
+const mapDispatchToPropsActions=(dispatch)=>({
+  setCity: value =>dispatch(setCity(value))
+ });
+const AppConnected = connect(null,mapDispatchToPropsActions)(App);
+
+export default AppConnected;
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
 
 export const SET_CITY = 'SET_CITY';
+// este es un actionCreator que le llamamos setCity
 export const setCity = value=>({type: SET_CITY, value});
