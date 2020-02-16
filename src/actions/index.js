a172bfce2daf4768ee927b8ffd251ca829bcd896
javierblancofernandez <javibf50@hotmail.com>
2020-02-16 19:14:50 +0100

    making reducer city

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
 
 export const SET_CITY = 'SET_CITY';
 // este es un actionCreator que le llamamos setCity
-export const setCity = value=>({type: SET_CITY, value});
+export const setCity = payload=>({type: SET_CITY, payload});
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,8 @@
 import { createStore } from "redux";
+import {city} from '../reducers/city';
+//generamos el estado inicial de la aplicación
+const initialState = {}
+//generamos el reducer que tiene 2 params state(estado de la app) y las acciones que generamos
 
-export const store = createStore(() => {},
+export const store = createStore(city,initialState,
 window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
